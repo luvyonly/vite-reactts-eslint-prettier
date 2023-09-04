b02711df142c978a554b9cbd3240ce397f052e2e
luvyonly <luvyonly@gmail.com>
2023-09-05 02:34:12 +0900

    1231

--- src/pages/Container/Styled.ts
+++ src/pages/Container/Styled.ts
@@ -2,16 +2,16 @@ import styled from '@emotion/styled';
 // import { Unit } from "../../customs/index";
 
 export const Container = styled.div`
-	padding-top: 36px;
+  padding-top: 36px;
 `;
 
 export const Title = styled.h1`
-	background-color: hotpink;
-	font-size: 24px;
-	border-radius: 4px;
-	padding: 32px;
-	text-align: center;
-	&:hover {
-		color: white;
-	}
+  background-color: hotpink;
+  font-size: 24px;
+  border-radius: 4px;
+  padding: 32px;
+  text-align: center;
+  &:hover {
+    color: white;
+  }
 `;
--- src/pages/Container/index.tsx
+++ src/pages/Container/index.tsx
@@ -1,13 +1,14 @@
 import React from 'react';
-import * as S from './Styled';
+
 import Category from '../../components/Category';
+import * as S from './Styled';
 
 function Container() {
-	return (
-		<S.Container>
-			<Category />
-		</S.Container>
-	);
+  return (
+    <S.Container>
+      <Category />
+    </S.Container>
+  );
 }
 
 export default Container;
